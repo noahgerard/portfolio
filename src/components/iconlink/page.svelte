9cff020790e87b7454018b2bfe77f37871b4c8ff
noahgerard <helix.dev.xyz@gmail.com>
2024-05-23 17:59:44 -0400

    feat: fancy buttons

--- src/components/iconlink/page.svelte
+++ src/components/iconlink/page.svelte
@@ -8,6 +8,6 @@
 	console.log(icon);
 </script>
 
-<a target="_blank" href={link} class="flex transition-all ease-in-out w-fit hover:text-[#A79277] hover:rotate-3 hover:scale-[1.1]">
+<a target="_blank" href={link} class="flex transition-all ease-in-out w-fit hover:text-primary hover:rotate-3 hover:scale-[1.1]">
 	<Icon icon={icon} class="w-8 h-8" />
 </a>
